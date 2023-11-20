ba552a7506b7fc6f2ec77fa753c4274230fa15e6
Harris Khan <32182515+Harris-A-Khan@users.noreply.github.com>
2023-11-20 13:08:58 -0500

    feat: count each protocol

--- assignment2-scapy.py
+++ assignment2-scapy.py
@@ -1,15 +1,33 @@
-from scapy.all import sniff, conf
+from scapy.all import sniff, IP, TCP, UDP, DNS
 
-# def process_packet(packet): 
-#     print(packet.summary())
+def process_packet(packet):
+    # Increment protocol counters based on packet type
+    if IP in packet:
+        protocol_counts['IP'] += 1
+    if TCP in packet:
+        protocol_counts['TCP'] += 1
+    if UDP in packet:
+        protocol_counts['UDP'] += 1
+    if DNS in packet:
+        protocol_counts['DNS'] += 1
 
-# def main(): 
-#     print("Starting sniffer...")
-#     # conf.verb = 0
-#     # sniff(iface="en0",prn=process_packet, timeout=30)
-#     # print(sniff.__doc__)
+def main(): 
+    print("Starting sniffer...")
+    
+    # Initialize protocol counts
+    global protocol_counts
+    protocol_counts = {'IP': 0, 'TCP': 0, 'UDP': 0, 'DNS': 0}
 
+    # Run sniff for a specific duration
+    duration = 5  # Duration in seconds
+    sniff(timeout=duration, prn=process_packet)
+    
+    # Let the user know we're finished sniffing 
+    print("Finished sniffing.")
 
+    print("Protocol counts:")
+    for protocol, count in protocol_counts.items():
+        print(f"{protocol}: {count}")
 
-# if "__name__" == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
