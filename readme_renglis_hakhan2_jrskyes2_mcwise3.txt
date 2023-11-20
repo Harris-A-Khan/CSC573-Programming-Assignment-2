0ba3a17f4e5dfee2c72b1843b12200eee0110aea
Harris Khan <32182515+Harris-A-Khan@users.noreply.github.com>
2023-11-20 14:46:32 -0500

    feat: finish project 2 code assignment

---
+++ readme_renglis_hakhan2_jrskyes2_mcwise3.txt
@@ -0,0 +1,19 @@
+PREREQUISITES: 
+
+1) Python installed on test machine 
+2) Pip installed on test machine
+
+
+INSTALLATION:
+
+1) Clone the repository to your local machine
+2) Navigate to the root directory of the repository
+3) Run the following command to install the required packages:
+
+pip install scapy or pip3 install scapy
+
+4) Run the following command to run the program:
+
+python3 sniffer_renglis_hakhan2_jrskyes2_mcwise3.py
+
+The program is running!
--- sniffer_renglis_hakhan2_jrskyes2_mcwise3.py
+++ sniffer_renglis_hakhan2_jrskyes2_mcwise3.py
@@ -1,3 +1,8 @@
+# Programming Assignment 2
+# Author: Robert English (renglis), Harris Khan (hakhan2), Jackson Skyes (jrskyes2), Madeline Wise (mcwise3)
+# Date: November 28th, 2023
+# Description: This is a packet sniffing program that will sniff packets and count the number of packets in a specific protocol (IP, TCP, UDP, and DNS)
+
 from scapy.all import sniff, IP, TCP, UDP, DNS
 
 def process_packet(packet):
@@ -14,12 +19,12 @@ def process_packet(packet):
 def main(): 
     print("Starting sniffer...")
     
-    # Initialize protocol counts
+    # Initialize protocol counts dictionary
     global protocol_counts
     protocol_counts = {'IP': 0, 'TCP': 0, 'UDP': 0, 'DNS': 0}
 
     # Run sniff for a specific duration
-    duration = 4  # Duration in seconds
+    duration = 30  # Duration in seconds
     sniff(timeout=duration, prn=process_packet)
     
     # Let the user know we're finished sniffing 
@@ -29,8 +34,9 @@ def main():
     for protocol, count in protocol_counts.items():
         print(f"{protocol}: {count}")
 
+    # TODO: properly name the files with everyones unity ID
     print("Writing to file...")
-    with open('sniffer.csv', 'w') as file: 
+    with open('sniffer_renglis_hakhan2_jrskyes2_mcwise3.csv', 'w') as file: 
         file.write("protocol,count\n")
         for protocol, count in protocol_counts.items():
             file.write(f"{protocol.lower()},{count}\n")
