24905521ed1ab46165d81175a1c95ceb461ce6c2
Harris Khan <32182515+Harris-A-Khan@users.noreply.github.com>
2023-11-20 11:12:20 -0500

    feat: start writing scapy version of program

---
+++ assignment2-scapy.py
@@ -0,0 +1,15 @@
+from scapy.all import sniff, conf
+
+# def process_packet(packet): 
+#     print(packet.summary())
+
+# def main(): 
+#     print("Starting sniffer...")
+#     # conf.verb = 0
+#     # sniff(iface="en0",prn=process_packet, timeout=30)
+#     # print(sniff.__doc__)
+
+
+
+# if "__name__" == "__main__":
+#     main()
