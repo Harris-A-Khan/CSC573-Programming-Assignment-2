f0e673216b44638e071f0373faf5b4e26a80e9d1
Harris Khan <32182515+Harris-A-Khan@users.noreply.github.com>
2023-11-20 10:51:01 -0500

    feat: start writing code

--- assignment2.py
+++ assignment2.py
@@ -3,3 +3,18 @@
 # Date: November 28th, 2023
 # Description: This is a packet sniffing program that will sniff packets and count the number of packets in a specific protocol (IP, TCP, UDP, and DNS)
 
+# Importing the necessary libraries
+from socket import socket, AF_PACKET, SOCK_RAW, htons
+
+# GLOBAL VARS
+ALL_PROTOCOLS = 0x0003
+
+# Create a low-level socket that listens to all packets on the machine
+rawSocket = socket(AF_PACKET, SOCK_RAW, socket.htons(ALL_PROTOCOLS))
+
+
+def main(): 
+    print("Starting packet sniffer...")
+
+if __name__ == "__main__":
+    main()
