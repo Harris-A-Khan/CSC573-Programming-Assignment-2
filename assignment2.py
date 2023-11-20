044d3b7171edab7a63ac0ccff7b8675b3952e4db
Harris Khan <32182515+Harris-A-Khan@users.noreply.github.com>
2023-11-20 10:39:16 -0500

    feat: begin the assignment

---
+++ assignment2.py
@@ -0,0 +1,5 @@
+# Programming Assignment 2
+# Author: Robert English, Harris Khan, Jackson Skyes, Madeline Wise
+# Date: November 28th, 2023
+# Description: This is a packet sniffing program that will sniff packets and count the number of packets in a specific protocol (IP, TCP, UDP, and DNS)
+
